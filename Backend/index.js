27963f5a52456190d456a0c6407aaa4b0a795489
4ist <frichardson062@gmail.com>
2020-07-08 05:33:09 -0400

    forked from test repo

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 console.log("Hello worlt");
-
 const http = require("http");
 const express = require("express");
+const { setupMaster } = require("cluster");
 const app = express();
 
 let notes = [
@@ -25,15 +25,106 @@ let notes = [
   },
 ];
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
-});
+setup()
+defineRoutes()
+launch()
 
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
-});
+function setup(){
+  
+  app.use(express.json());
+  
+  const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+  }
+  
+  app.use(requestLogger)
+}
 
-const PORT = 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+function defineRoutes(){
+  setupGets()
+  setupPosts()
+  setupDeletes()
+  setupUnknownEndpoint()
+}
+
+function setupGets(){
+  app.get("/", (req, res) => {
+    res.send("<h1>Hello World!</h1>");
+  });
+  
+  app.get("/api/notes", (req, res) => {
+    res.json(notes);
+  });
+  
+  app.get("/api/notes/:id", (request, response) => {
+    const id = request.params.id;
+    const note = notes.find((note) => {
+      console.log(note.id, typeof note.id, id, typeof id, note.id === id);
+      return note.id == id;
+    });
+    console.log(note);
+    if (note) {
+      response.json(note);
+    } else {
+      response.status(404).end();
+    }
+  });
+}
+function setupPosts(){
+  app.post('/api/notes', (request, response) => {
+    const body = request.body
+  
+    if (!body.content) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+  
+    const note = {
+      content: body.content,
+      important: body.important || false,
+      date: new Date(),
+      id: generateId(),
+    }
+    notes = notes.concat(note)
+    response.json(note)
+  })
+
+  const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+}
+
+function setupDeletes(){
+  app.delete("/api/notes/:id", (request, response) => {
+    const id = Number(request.params.id);
+    notes = notes.filter((note) => note.id !== id);
+  
+    response.status(204).end();
+  });
+}
+
+function setupUnknownEndpoint(){
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  app.use(unknownEndpoint)
+}
+
+function launch(){
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+
+
+
